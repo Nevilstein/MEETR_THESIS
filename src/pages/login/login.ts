318045c3d9d1d7366dab48df002a73b9129b8c2c
sopran123 <sfhsiono@gmail.com>
2018-11-25 13:45:40 +0800

    facebook login firebase
    error in importing firebase

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import firebase from 'firebase';
+import { Firebase } from '@ionic-native/firebase';
+import { Facebook } from '@ionic-native/facebook';
 /**
  * Generated class for the LoginPage page.
  *
@@ -15,11 +17,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public facebook: Facebook) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  facebookLogin(): Promise<any> {
+    return this.facebook.login(['email']);
+    .then( response => {
+      const facebookCredential = firebase.auth.FacebookAuthProvider
+        .credential(response.authResponse.accessToken);
+
+      firebase.auth().signInWithCredential(facebookCredential)
+        .then( success => { 
+          console.log("Firebase success: " + JSON.stringify(success)); 
+        });
+
+    }).catch((error) => { console.log(error) });
   }
 
 }
