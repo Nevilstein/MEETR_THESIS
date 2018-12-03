a3d35923f13c527cdac02243ef1f9005771798dd
Nevilstein <keikirishima891@gmail.com>
2018-12-03 16:55:06 +0800

    fix merge conflict v2

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -5,11 +5,7 @@ import { LoginPage } from './login';
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
-    LoginPage,
-=======
     //LoginPage,
->>>>>>> 4b32c90... user page
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
